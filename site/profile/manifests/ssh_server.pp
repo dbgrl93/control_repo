b0919b588d0e44dabc5d9d7a5b1ae37dcefc0a1d
dbgrl93 <34557134+dbgrl93@users.noreply.github.com>
2020-03-21 16:41:16 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDiRHoghWKtXo+7AIOwOjGjCK3A6XyqmozP1SwhBI53+HPHaDgAQyLyaASP0gnqXOgz2yIfrA/2L2VHjDU+Z/plQTCduJlpGk6VlUEgCF8b4Z+0V0lKEteuPtXMgm4w3Ir5e7JfSRPmqPeBWMp8to8EcxPNavDKqM6rWO7S4SDsgMB8masowfGHZWgn4Zr6b29nMBfG8GgQgr1jxQYicxKWCJOis+Fr3ZPEPHtlUOeNvLsBUSSxBw7CPA/m6yEBz/WHcZFqC40H3pM3ORkHz3/E0BuEzj+wZQ9XbgE3XIdjo8SkdvdmQp209gaWyvvehdEnubEhOgXY+kPAdoabihC1',
 	}  
 }
